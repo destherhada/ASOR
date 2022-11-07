1e2abe2fa4fccaa4c98687b4afed661c37ce95b0
destherhada <48451168+destherhada@users.noreply.github.com>
2022-11-07 09:54:15 +0100

    Update ejercicio1.cc

--- practica2.1/ejercicio1.cc
+++ practica2.1/ejercicio1.cc
@@ -1,1 +1,12 @@
+//Practica 2.1 -- Ejercicio 1 -- Esther Babon y Claudia Lopez-Mingo
 
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main() {
+    if (setuid(0) == -1){
+        perror("Error");
+    }
+    return 1;
+}
