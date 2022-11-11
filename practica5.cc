364aea006c39176df7442d66eb442d809d028149
destherhada <48451168+destherhada@users.noreply.github.com>
2022-11-11 19:46:50 +0100

    Create practica5.cc

---
+++ practica5.cc
@@ -0,0 +1,20 @@
+#include <sys/utsname.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+
+int main() {
+    struct utsname buf;
+    if(uname(&buf) == -1){
+      printf("ERROR(%d): %s\n", errno, strerror(errno));  
+    }
+    printf("Sysname: %s\n", buf.sysname);
+    printf("Nodename: %s\n", buf.nodename);
+    printf("Release: %s\n", buf.release);
+    printf("Version: %s\n", buf.version);
+    printf("DomainName: %s\n", buf.__domainname);
+    printf("Machine: %s\n", buf.machine);
+    return 1;
+}
