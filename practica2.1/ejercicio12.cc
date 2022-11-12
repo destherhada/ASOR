f007bfda9f1c7e9d29c1c504b95b632f999da958
destherhada <48451168+destherhada@users.noreply.github.com>
2022-11-12 18:56:32 +0100

    Create ejercicio12.cc

---
+++ practica2.1/ejercicio12.cc
@@ -0,0 +1,9 @@
+//Practica 2.1 -- Ejercicio 12 -- Esther Babon y Claudia Lopez-Mingo
+#include <time.h>
+#include <stdio.h>
+
+int main(){
+    printf("Hora desde el Epoch en segundos: %li\n", time(NULL));
+
+    return 1;
+}
