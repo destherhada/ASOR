35448b8f959ea9d988878fa4166a5416edf130da
destherhada <48451168+destherhada@users.noreply.github.com>
2022-11-12 18:23:55 +0100

    Update ejercicio6.cc

--- practica2.1/ejercicio6.cc
+++ practica2.1/ejercicio6.cc
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int main(){
-    printf("Longitud maxima de argumentos: %s\n", sysconf(_SC_ARG_MAX));
-    printf("Numero maximo de hijos: %s\n", sysconf(_SC_CHILD_MAX));
-    printf("Numero maximo de ficheros abiertos: %s\n", sysconf(_SC_OPEN_MAX));
+    printf("Longitud maxima de argumentos: %li\n", sysconf(_SC_ARG_MAX));
+    printf("Numero maximo de hijos: %li\n", sysconf(_SC_CHILD_MAX));
+    printf("Numero maximo de ficheros abiertos: %li\n", sysconf(_SC_OPEN_MAX));
 
     return 1;
 }
