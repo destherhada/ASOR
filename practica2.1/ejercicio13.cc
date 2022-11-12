0ae222458c41db5c8302db869f171569e654875c
Claudla3 <61540518+Claudla3@users.noreply.github.com>
2022-11-12 20:54:40 +0100

    Update ejercicio13.cc

--- practica2.1/ejercicio13.cc
+++ practica2.1/ejercicio13.cc
@@ -1,1 +1,20 @@
+#include <sys/time.h>
 
+int main(){
+  struct timeval tv; //Para medir el tiempo antes del bucle
+  struct timeval tv2; //Para medir el tiempo después del bucle
+  int var = 0;        //Variable a incrementar
+  int total = 0;      //Variable auxiliar para cálculos
+  gettimeofday(&tv, NULL);    //Sacamos tiempo antes de bucle
+  while (var < 1000000){
+    var++;            //Incrementamos variable
+  }
+  gettimeofday(&tv2, NULL);   //Sacamos tiempo después de bucle
+  
+  total += tv2.tv_usec - tv.tv_usec;  //Calculamos los microsegundos
+  total += (tv2.tv_sec - tv.tv_sec)*1000000;
+  //Imprimimos en pantalla
+  printf("Tiempo en incrementar variable: %s\n", total);
+  
+  return 1;
+}
