f27687f7325653c2469da1e43e7603d59b375495
Claudla3 <61540518+Claudla3@users.noreply.github.com>
2022-11-30 11:46:17 +0100

    Update ejercicio5.c

--- practica2.3/ejercicio5.c
+++ practica2.3/ejercicio5.c
@@ -1,17 +1,27 @@
-//Práctica 2.3 -- Ejercicio 3 -- Esther Babon Arcauz y Claudia Lopez-Mingo Morenp
+//Práctica 2.3 -- Ejercicio 5 -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
 #include <sched.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main(){
     pid_t pid = getpid();
     pid_t ppid = getppid();
     pid_t pgid = getpgid(pid);
     pid_t sid = getsid(pid);
-    char ruta;
-    getcwd(ruta, 1024);
+    char *ruta = malloc(sizeof(char)*(2048));
+    getcwd(ruta, 2048);
+    struct rlimit limit;
+    getrlimit(RLIMIT_NOFILE, &limit);
+
+    printf("PID: %i\n", pid);
+    printf("PPID: %i\n", ppid);
+    printf("PGID: %i\n", pgid);
+    printf("SID: %i\n", sid);
+    printf("Directorio de trabajo actual: %s\n", ruta);
+    printf("Número máximo de ficheros: %li\n", limit.rlim_max);
 
     return 1;
 }
