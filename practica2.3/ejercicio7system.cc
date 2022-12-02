21262783d1235da85384b34c76b8b9c18e279f36
destherhada <48451168+destherhada@users.noreply.github.com>
2022-12-02 18:27:32 +0100

    Create ejercicio7system.cc

---
+++ practica2.3/ejercicio7system.cc
@@ -0,0 +1,25 @@
+//Practica 2.3 -- Ejercicio 7 System -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+
+    int length = 1;
+    for(int i = 1; i < argc; i++){
+        length += strlen(argv[i]) + 1;
+    }
+
+    char *command = (char *)malloc(sizeof(char) * length);
+    strcpy(command, "");
+
+    for (int i = 1; i < argc; i++){
+        strcat(command, argv[i]);
+        strcat(command, " ");
+    }
+
+    system(command);
+    printf("El comando termino de ejecutarse.");
+
+    return 1;
+}
