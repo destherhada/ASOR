dcce66cd2689ac0b979abbf9dd881701fafab52e
destherhada <48451168+destherhada@users.noreply.github.com>
2022-12-17 20:09:03 +0100

    Update ejercicio2.cc

--- practica2.4/ejercicio2.cc
+++ practica2.4/ejercicio2.cc
@@ -1,1 +1,64 @@
 
+//Practica 2.4 -- Ejercicio 2 -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(){
+
+    int p_h[2];
+    int h_p[2];
+    pipe(p_h);
+    pipe(h_p);
+    char pmsg[1] = {'l'};
+
+    switch (fork()){
+        case 0:
+            close(p_h[1]);
+            close(h_p[0]);
+            char rmsg[1025];
+            
+
+            for(int i = 0; i < 11; i++){
+                ssize_t size = read(p_h[0], rmsg, 1024);
+                rmsg[size] = '\0';
+                printf("[Hijo] Mensaje : %s", rmsg);
+                sleep(1);
+
+                if(i == 9){
+                    pmsg[0] = 'q';
+                }
+                size = write(h_p[1], pmsg, 1);
+            }
+            close(p_h[0]);
+            close(h_p[1]);
+
+            exit(0);
+            break;
+
+
+        default:
+            close(p_h[0]);
+            close(h_p[1]);
+            char msg[1025];
+            char cmsg[1] = {'l'};
+
+            while (cmsg[0] != 'q'){
+                printf("[Padre] Escribe nuevo mensaje: \n");
+                ssize_t psize = read(0, msg, 1024);
+                msg[psize] = '\0';
+                psize = write(p_h[1], msg, psize + 1);
+            
+            }
+            close(p_h[1]);
+			close(h_p[0]);
+
+            exit(0);
+
+
+    }
+
+  return 1;
+}
