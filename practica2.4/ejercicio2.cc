77ed36b31ad9570989da4d17354ab0d0a9828d69
destherhada <48451168+destherhada@users.noreply.github.com>
2022-12-17 20:40:07 +0100

    Update ejercicio2.cc

--- practica2.4/ejercicio2.cc
+++ practica2.4/ejercicio2.cc
@@ -1,4 +1,3 @@
-
 //Practica 2.4 -- Ejercicio 2 -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
 #include <stdio.h>
 #include <signal.h>
@@ -20,7 +19,6 @@ int main(){
             close(h_p[0]);
             char rmsg[1025];
             
-
             for(int i = 0; i < 10; i++){
                 ssize_t size = read(p_h[0], rmsg, 1024);
                 rmsg[size] = '\0';
@@ -34,7 +32,6 @@ int main(){
             }
             close(p_h[0]);
             close(h_p[1]);
-
             exit(0);
             break;
 
@@ -50,16 +47,17 @@ int main(){
                 ssize_t psize = read(0, msg, 1024);
                 msg[psize] = '\0';
                 psize = write(p_h[1], msg, psize + 1);
+                if (psize == -1) {
+					perror("[Padre] Error al escribir en la tuberia");
+					exit(-1);
+				}
                 while (cmsg[0] != 'l' && cmsg[0] != 'q') {
 					psize = read(h_p[0], cmsg, 1);
 				}
             }
             close(p_h[1]);
 			close(h_p[0]);
-
             exit(0);
-
-
     }
 
   return 0;
