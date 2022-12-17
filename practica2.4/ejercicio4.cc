f936ea71dcb8f624a58fbd7b6c6e6ba75157c0ab
destherhada <48451168+destherhada@users.noreply.github.com>
2022-12-17 20:56:19 +0100

    Update ejercicio4.cc

--- practica2.4/ejercicio4.cc
+++ practica2.4/ejercicio4.cc
@@ -1,1 +1,22 @@
+//Practica 2.4 -- Ejercicio 4 -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
+
+
+int main(int argc, char **argv){
+
+    mkfifo("./tuberia", 0777);
+    int tub = open("./tuberia", O_WRONLY);
+    size_t len = strlen(argv[1]);
+    ssize_t w = write(tub, argv[1], len);
+    close(tub);
+
+
+  return 0;
+}
