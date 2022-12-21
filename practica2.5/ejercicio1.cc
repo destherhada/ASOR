5cb577940473f2a5afe81e70d296f5ccd62c7887
Claudla3 <61540518+Claudla3@users.noreply.github.com>
2022-12-21 19:13:21 +0100

    Update ejercicio1.cc
    No funciona para www.google.com

--- practica2.5/ejercicio1.cc
+++ practica2.5/ejercicio1.cc
@@ -1,1 +1,32 @@
+//Practica 2.5 -- Ejercicio 1 -- Esther Babon Arcauz y Claudia Lopez-Mingo Moreno
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main (int argc, char**argv) {
+
+    struct addrinfo hints;
+    struct addrinfo *res, *ite;
+    
+    memset(&hints, 0, sizeof(struct addrinfo));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(argv[1], NULL, &hints, &res) != 0) {
+        printf("Error getaddrinfo(): Name or service not known");
+        exit(-1);
+    }
+
+    for (ite = res; ite != NULL; ite = ite->ai_next) {
+        char servi[NI_MAXHOST];
+        getnameinfo(ite->ai_addr, ite->ai_addrlen, servi, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+        printf("%s\t%i\t%i\n", servi, ite->ai_family, ite->ai_socktype);
+	}
+
+    freeaddrinfo(res);
+    return 0;
+}
